6f68736e829f970086dd68b011e4e9e297bdcbbb
Jake Hodgson <liveactiongamers@live.ca>
2011-11-21 15:07:23 -0800

    Copied Code from TestBlock Code and renamed ore1. Placeholder.txt Deleted

---
+++ src/ore1/mod_ore1.java
@@ -0,0 +1,37 @@
+package net.minecraft.src;
+
+import java.util.*;
+
+public class mod_ore1 extends BaseMod
+{
+
+	public static Block ore1 = new ore1(255, 0).setHardness(1.0f).setResistance(6000.0F).setLightValue(1.0F).setBlockName("ore1");
+
+	public String Version()
+	{
+		return "1.0";
+	}
+	
+	public mod_ore1()
+	{
+		ModLoader.RegisterBlock(ore1);
+		ore1.blockIndexInTexture = ModLoader.addOverride("/terrain.png", "/bph.png");
+		ModLoader.AddName(ore1, "ore1");
+		ModLoader.AddRecipe(new ItemStack(ore1, 1), new Object[] {
+			"###", "###", "###", Character.valueOf('#'), Item.redstone
+		});
+	}
+    public void GenerateSurface(World world, Random rand, int chunkX, int chunkZ)
+    {
+        for(int i = 0; i < 15; i++)
+        {
+            int randPosX = chunkX + rand.nextInt(16);
+            int randPosY = rand.nextInt(40);
+            int randPosZ = chunkZ + rand.nextInt(16);
+            (new WorldGenMinable(mod_ore1.ore1.blockID, 10)).generate(world, rand, randPosX, randPosY, randPosZ);
+        }
+    }   
+
+
+}
+
