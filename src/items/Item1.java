75e1373c40f5f40be7b8e57b9a89f1ee7b2807da
Darkwizard98 <liveactiongamers@live.ca>
2011-11-21 18:12:19 -0800

    Changed code for Item1 instead of TestItem
    Signed-off-by: Darkwizard98 <liveactiongamers@live.ca>

--- src/items/Item1.java
+++ src/items/Item1.java
@@ -2,11 +2,11 @@ package net.minecraft.src;
 
 import java.util.Random;
 
-public class TestItem extends Item
+public class Item1 extends Item
 {
 	
 	
-	public TestItem (int i)
+	public Item1 (int i)
 	{
 		super(i);
 		maxStackSize = 64;
--- src/items/mod_Item1.java
+++ src/items/mod_Item1.java
@@ -1,14 +1,14 @@
 package net.minecraft.src;
 
-public class mod_TestItem extends BaseMod
+public class mod_Item1 extends BaseMod
 {
-	public static final Item TestItem = new Item(30000).setItemName("TestItem");
+	public static final Item Item1 = new Item(30000).setItemName("Item1");
 	
-public mod_TestItem()
+public mod_Item1()
 {
-	TestItem.iconIndex = ModLoader.addOverride("gui/items.png", "/testi.png");
-	ModLoader.AddName(TestItem, "Test Item");
-	ModLoader.AddRecipe(new ItemStack(TestItem, 1), new Object[] {
+	Item1.iconIndex = ModLoader.addOverride("gui/items.png", "/testi.png");
+	ModLoader.AddName(Item1, "Test Item");
+	ModLoader.AddRecipe(new ItemStack(Item1, 1), new Object[] {
 		"XPX", "PSP", "XPX", Character.valueOf('P'), Item.porkCooked, Character.valueOf('X'), Item.redstone,
 		Character.valueOf('S'), Item.swordStone
 	});
