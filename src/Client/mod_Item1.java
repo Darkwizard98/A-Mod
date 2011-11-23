c9afd2c2d3294df39fbae4231870fe0cd3fb9648
Darkwizard98 <liveactiongamers@live.ca>
2011-11-23 11:51:57 -0800

    Update src/Client/mod_Item1.java

--- src/Client/mod_Item1.java
+++ src/Client/mod_Item1.java
@@ -7,7 +7,7 @@ public class mod_Item1 extends BaseModMp
 public mod_Item1()
 {
 	Item1.iconIndex = ModLoader.addOverride("/gui/items.png", "/imgz/items/ls.png");
-	ModLoader.AddName(Item1, "Item1");
+	ModLoader.AddName(Item1, "Lightstone Ball");
 	ModLoader.AddRecipe(new ItemStack(Item1, 1), new Object[] {
 		"XPX", "PSP", "XPX", Character.valueOf('P'), Item.porkCooked, Character.valueOf('X'), Item.redstone,
 		Character.valueOf('S'), Item.swordStone
